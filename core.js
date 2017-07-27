222a160212c69eeb7b8f3544d1a5210164e64c2e
Chad Eubanks <ceubanks@thecodeboutique.com>
2017-07-26 17:06:42 -0700

    Start of Alto base class

---
+++ core.js
@@ -0,0 +1,14 @@
+// ==========================================================================
+// Project: Alto - JavaScript Application Framework
+// Copyright: @2017 The Code Boutique, LLC
+// License:   Intellectual property of The Code Boutique. LLC
+// Author: Chad Eubanks
+// ==========================================================================
+
+class Alto {
+
+};
+
+window.Alto = new Alto();
+
+export default window.Alto;
