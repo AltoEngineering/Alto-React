a19fbbb310ea19d380ce11071e4930e42af95f3d
Chad Eubanks <ceubanks@thecodeboutique.com>
2017-07-26 17:06:55 -0700

    Adds application.js

---
+++ application/application.js
@@ -0,0 +1,30 @@
+// ==========================================================================
+// Project: Alto - JavaScript Application Framework
+// Copyright: @2017 The Code Boutique, LLC
+// License:   MIT
+// Author: Chad Eubanks
+// ==========================================================================
+
+let Application = class Application {
+
+    static toString() {
+        return 'Alto.Application'
+    }
+
+    static create(mixin) {
+        const application = Object.assign(new Application(), mixin);
+        application.applicationWillLoad();
+        return application;
+    }
+
+    applicationWillLoad () {
+        console.log('applicationWillLoad');
+    }
+
+    applicationDidLoad() {
+        console.log('applicationDidLoad');
+    }
+
+};
+
+export default Application;
